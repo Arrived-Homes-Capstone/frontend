ab689937c24ee8224c736cbb04735052c1ba42df
kusche12 <kylerkusche@gmail.com>
2021-05-27 18:23:58 -0700

    no auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 import AuthLandingPage from './auth/AuthLandingPage';
 import reportWebVitals from './reportWebVitals';
@@ -8,16 +9,8 @@ import './index.css';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-console.log(window.location.origin + '/frontend')
-
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin + '/frontend'}
-  >
-    <AuthLandingPage />
-  </Auth0Provider>,
+  <App />,
   document.getElementById('root')
 );
 
